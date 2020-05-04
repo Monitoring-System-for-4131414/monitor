844e3239b7a4f64413da5dda780fa5f974f83bd1
xzmt <xzmt_kz@hotmail.com>
2020-05-04 14:32:49 +0800

    项目初始化

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+DEBUG = True
+
+SECRET_KEY = os.urandom(24)
+DIALECT = 'mysql'
+DRIVER = 'mysqldb'
+USERNAME = 'root'
+PASSWORD = ''
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'monitor'
+
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
