9ccd5ae6da12b8f038286907a6d1583a3eac3714
Starplatinum977 <974420962@qq.com>
2020-05-06 08:52:11 +0800

    html网页部分

--- monitor.py
+++ monitor.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 import config
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ app.config.from_object(config)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/warning')
+def warning():
+    return render_template('warning.html')
+
+@app.route('/prompting')
+def prompting():
+    return render_template('prompting.html')
+
 
 
 if __name__ == '__main__':
